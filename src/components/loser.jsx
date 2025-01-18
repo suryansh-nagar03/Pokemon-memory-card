0736a78051661a43421d6788d3896c90d6287fa2
Suryansh Nagar <nagarsuryansh72@gmail.com>
2025-01-19 03:20:01 +0530

    improved loser overlay

--- src/components/loser.jsx
+++ src/components/loser.jsx
@@ -5,9 +5,9 @@ export default function Loser({score,resetGame}) {
         <div className='flex items-center justify-center fixed inset-0 bg-black/30 backdrop-blur-sm z-50 pt-20 pb-20 rounded-lg'>
             <div className='bg-white rounded-2xl p-8 shadow-lg w-4/12 h-full'>
                 <div className='flex flex-col justify-center align-center items-center gap-4'>
-                    <div className='text-2xl font-bold mb-4 font-pixel'>You Lost</div>
+                    <div className='text-2xl font-bold font-pixel'>You Lost</div>
                     <img src={win} alt="win gift" className='h-48'/>
-                    <div className='text-sm my-4 font-pixel'>Your final score is: {score}</div>
+                    <div className='text-sm my-2 font-pixel'>Your final score is: {score}</div>
                     <button 
                         type="button" 
                         onClick={resetGame}
