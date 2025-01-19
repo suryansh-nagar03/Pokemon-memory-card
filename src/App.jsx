25a9f57d00ed7fc75a437f40b1936073d82821ea
Suryansh Nagar <nagarsuryansh72@gmail.com>
2025-01-20 01:17:06 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import background from "./assets/background.jpg";
 import Header from "./components/header";
 import Card from "./components/card";
 import Winner from "./components/winner";
